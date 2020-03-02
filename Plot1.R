754fd7b72b10d246c5261d57770a3a23f39621ea
Willianto Asalim <willianto@calibre.com.au>
2020-03-02 16:47:14 +1100

    histogram plot

--- Plot1.R
+++ Plot1.R
@@ -7,3 +7,11 @@ data$date<-as.Date(data$Date, "%d/%m/%Y")
 #Subset the required data from 2007-02-01 to 2007-02-02
 requiredData <- subset(data, data$date >= "2007-02-01" & data$date <= "2007-02-02")
 
+#plot1 png file
+png("plot1.png", width=480, height=480)
+
+#Create the plot 1 histogram
+hist(requiredData$Global_active_power, main="Global Active Power", xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
+
+#Turn off the screen if it is no longer in use
+dev.off()
